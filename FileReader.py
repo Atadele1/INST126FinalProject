1fe89c83f67141875548c9a46b2dc8040a39a7b0
Abigiya <atadele@terpmail.umd.edu>
2023-12-16 23:47:44 -0500

    Change

--- FileReader.py
+++ FileReader.py
@@ -1,25 +1,25 @@
 import os
 import re
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 # Step 1: Read Multiple HTML Files
-folder_path = 'C:\Users\goodr\Downloads\INST126FinalProject\HTMLFiles'
+folder_path = r'\Users\goodr\Downloads\INST126FinalProject\HTMLFiles'
 html_files = [file for file in os.listdir(folder_path) if file.endswith('.html')]
 
-word_count = {}  # Dictionary to store count of specific words
+word_count = {}  
 
-# Step 2: Search for Specific Words (Case Sensitive)
-target_word = 'your_target_word'  # Replace 'your_target_word' with the word you're looking for
+#Searching for specific words that are case sensitive Won, Nominated, Runner Up
+target_word = 'Won'  
 
 for file_name in html_files:
     with open(os.path.join(folder_path, file_name), 'r') as file:
-        html_content = file.read()
+        html_content = file.read
 
-    # Search for the target word using regular expressions
-    count = len(re.findall(r'\b' + re.escape(target_word) + r'\b', html_content))
+    
+    count = len(target_word)
     word_count[file_name] = count
 
-# Step 3: Using Matplotlib to Show Files with Most Case Sensitive Words
+# Matplotlib to Show Files with Most Case Sensitive Words
 file_names = list(word_count.keys())
 counts = list(word_count.values())
 
@@ -29,6 +29,6 @@ plt.xlabel('HTML Files')
 plt.ylabel(f'Occurrences of "{target_word}" (Case Sensitive)')
 plt.title(f'Occurrences of "{target_word}" in HTML Files')
 
-plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+plt.xticks(rotation=45, ha='right') 
 plt.tight_layout()
 plt.show()
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # INST126FinalProject
 <<<<<<< HEAD
-This project will read Wikipedia plots pages of the highest grossing movies in last year (of 2023) to see how many nomination each film received.
+This project will read Wikipedia plots pages of the highest grossing movies in last year (of 2023) to see how many won each film received.
 
 <!-- Name:  -->
 <!-- UID:  -->
 =======
 This project will read Wikipedia plots pages of the highest grossing movies in last year (of 2023) to see how many nomination each film received
 
-# UID: 
-# Name:  
+# UID: 117671353
+# Name:  Abigiya Tadele
 >>>>>>> 556a2bf7fa2c7b6d7cfd82ec3cec71cda817b309
